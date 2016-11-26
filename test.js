4fdf5264a88638637da98818016e6842fc31fdf6
Sarmad Sangi <sarmadsangi@gmail.com>
2016-11-26 17:23:06 +0800

    init

---
+++ test.js
@@ -0,0 +1,8 @@
+// const getSoilMoistureReading = require('./index.js');
+//
+// setInterval(() => {
+//   getSoilMoistureReading((value) => {
+//     // value contains calibrated value and normal value
+//     console.log(value);
+//   });
+// }, 1200);
